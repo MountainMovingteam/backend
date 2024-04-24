f024e26e65517365fa6502a0349f09039d322724
卞宇轩 <21373151@buaa.edu.cn>
2024-04-24 16:36:42 +0800

    [feat](base):初步完成登录注册api

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class Student(models.Model):
+    student_id = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    name = models.CharField(max_length=32)
+    phone = models.CharField(max_length=32)
+    academy = models.IntegerField()
+    avatar = models.CharField(max_length=200)
+
+
+class Admin(models.Model):
+    staff_id = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+    name = models.CharField(max_length=32)
+    avatar = models.CharField(max_length=200)
+    is_super_admin = models.BooleanField()
