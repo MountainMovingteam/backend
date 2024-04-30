9c05ab219269e43e33bd69a356fc324cfdefc170
chenshigui <csg54200@outlook.com>
2024-04-30 20:12:07 +0800

    [fix](manager):fix manager

--- manager/lib/static_response.py
+++ manager/lib/static_response.py
@@ -2,40 +2,40 @@ from django.http import JsonResponse, HttpResponse
 
 
 def success_respond():
-    return JsonResponse({'success': True})
+    return JsonResponse({'success': True}, status=200)
 
 
 def role_wrong():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def none_token():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def login_timeout():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def user_not_exists():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def place_not_exists():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def order_not_exists():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def order_type_wrong():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def lecturer_has_exists():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
 
 
 def lecturer_not_exists():
-    return JsonResponse({'success': False})
+    return JsonResponse({'success': False}, status=404)
